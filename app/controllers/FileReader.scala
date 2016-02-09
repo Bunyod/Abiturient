11d44bf0f0f198fe78f46b8cfee1b6314ca6a46b
Bunyod <bunyodreal@gmail.com>
2016-02-09 16:32:21 +0500

    doc read and parsing

--- app/controllers/FileReader.scala
+++ app/controllers/FileReader.scala
@@ -6,6 +6,7 @@ package controllers
 
 
 import java.io.File
+import java.util.Date
 
 import org.apache.poi.openxml4j.opc.OPCPackage
 import org.apache.poi.xwpf.extractor.XWPFWordExtractor
@@ -17,11 +18,36 @@ import play.api.{Logger, Play}
 class FileReader extends Controller {
 
   def ind() = Action { implicit req =>
-    val docFile = new File(Play.getFile("public/"), "savol.docx")
+    val docFile = new File(Play.getFile("public/"), "savollar.docx")
     val docx = new XWPFDocument(OPCPackage.openOrCreate(docFile))
+    val picData = docx.getAllPictures.get(0).getData
+    val picName= docx.getAllPictures.get(0).getFileName
+    val picType = docx.getAllPictures.get(0).getPictureType
+
+
+    val paragraphs = docx.getParagraphs
+    for (i <- 0 to paragraphs.size()-1) {
+      val paragraph = paragraphs.get(i)
+      val element = paragraph.getRuns
+      for (i <- 0 to element.size() - 1) {
+        val embPic = element.get(i)
+        val pics= embPic.getEmbeddedPictures
+        val ss= embPic.text()
+        if (pics.size() > 0) {
+          val picName = pics.get(0).getPictureData.getFileName
+          val now = new Date()
+          embPic.setText(s"%%${now.getTime}$picName%% ")
+        }
+      }
+
+
+    }
+
     val wx = new XWPFWordExtractor(docx)
-    val text = wx.getText()
-    Logger.debug(s"TT = $text")
+
+    val tt = wx.getText
+
+    Logger.debug(s"TTTT = $tt")
 
     Ok("asdf")
   }
