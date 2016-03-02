33bac9507268e5d18e3d0ae9ed01b53cdba756a5
Bunyod <bunyodreal@gmail.com>
2016-03-02 10:50:56 +0500

    refactor

--- app/controllers/QuizController.scala
+++ app/controllers/QuizController.scala
@@ -8,9 +8,11 @@ import akka.util.Timeout
 import be.objectify.deadbolt.scala.{ActionBuilders, DeadboltActions}
 import com.typesafe.scalalogging.slf4j.LazyLogging
 import common.AppProtocol._
+import dao.UsersDao
 import play.api.Play._
 import play.api.libs.json.Json
 import play.api.mvc.{Action, Controller}
+import security.MyDeadboltHandler
 
 import scala.concurrent.ExecutionContext
 import scala.concurrent.duration.DurationInt
@@ -22,6 +24,7 @@ import scala.concurrent.duration.DurationInt
 
 class QuizController @Inject() (val actorSystem: ActorSystem,
                                 deadbolt: DeadboltActions,
+                                usersDao: UsersDao,
                                 actionBuilders: ActionBuilders)
                                (implicit ec: ExecutionContext) extends Controller with LazyLogging {
 
@@ -29,6 +32,12 @@ class QuizController @Inject() (val actorSystem: ActorSystem,
   val config = current.configuration.getConfig("web-server").get
   val quizManger = actorSystem.actorSelection(config.getString("quiz-manager-actor-path").get)
 
+
+  def tests = deadbolt.SubjectPresent(new MyDeadboltHandler(None, usersDao)) {
+    Action {
+      implicit request => Ok(views.html.logged_in.tests())
+    }
+  }
 
   def getQuestions() = Action.async { implicit request =>
 
